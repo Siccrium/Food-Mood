3160e677ddfd19f45c58ee46478592fb24017c55
Garrett L. Harvey <glharvey@go.olemiss.edu>
2019-04-02 02:54:06 -0500

    C.R.U.D Orders Done (needs changes when ordering from specific menu(s?)/something weird happens if you add multiple orders)

--- js/customer.js
+++ js/customer.js
@@ -2,14 +2,16 @@
 firestore = firebase.firestore();
 
 var email = "";
+//customers will need a unique id so we can allow them to edit their email. 
+//or just don't let them. either way, but I suggest uId
 
 const placeOrder = document.getElementById("placeOrder");
-const accountButton = document.getElementById("orderButton");
+// const accountButton = document.getElementById("accountButton");
 const ordersButton = document.getElementById("ordersButton");
 const notifyHeader = document.getElementById("notifyHeader");
-const orderList = document.getElementById("orderList");
 
 notifyHeader.style.visibility = "hidden";
+
 
 //gonna use this to show all orders that I have
 // function renderPage() {
@@ -56,16 +58,16 @@ placeOrder.addEventListener("click", e => {
   notifyHeader.innerText = "Order Placed!";
   notifyHeader.style.visibility = "visible";
   //instead of specific rest id, use vars restaurant_id of rest menu you are adding from
-  firestore.doc("Restaurants/RRWswvxp24gRBdBgYouD").get().then(function(doc){
-    console.log("get rest worked")
+  firestore.doc("Restaurants/PsB7bBjf63vmiOrKoc3M").get().then(function (doc) {
+    console.log("get restaurant worked")
 
     var restData = doc.data();
     console.log(restData);
     var managerEmail = restData.RestaurantManager;
-    var newOrderRef = firestore.collection("Restaurants/RRWswvxp24gRBdBgYouD/Orders/").doc();
+    var newOrderRef = firestore.collection("Restaurants/PsB7bBjf63vmiOrKoc3M/Orders/").doc();
     var orderInfo = {
       FoodItem: "Spaghetti",
-      AmountPaid: "$10",
+      AmountPaid: "$14.95",
       OrderStatus: "Pending",
       OrderAuthor: email,
       OrderManager: managerEmail,
@@ -73,47 +75,50 @@ placeOrder.addEventListener("click", e => {
     }
     newOrderRef.set(orderInfo).then(function () {
       console.log("Order successfully written.");
-      firestore.doc("Users/"+email+"/Orders/"+newOrderRef.id).set({
-        OrderNumber: newOrderRef.id,
-        OrderManager: managerEmail
+      firestore.doc("Users/" + email + "/Orders/" + newOrderRef.id).set({
+        OrderId: newOrderRef.id,
+        RestaurantId: doc.id
       })
-  }).catch(function (error) {
+    }).catch(function (error) {
       console.log("Error writing document: " + error + ".");
-  });
+    });
 
   });
-  
+
 
 })
 
+ordersButton.addEventListener("click", e => {
+  window.location.replace("orders.html");
+});
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in. Get their email.
-      console.log("The currently logged in user is: " + user.email + ".");
-      email = user.email;
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in. Get their email.
+    console.log("The currently logged in user is: " + user.email + ".");
+    email = user.email;
 
-      //Check to see if this user is a customer. If not, redirect them to their dashboard.
-      firestore.doc("/Users/" + email).get().then(function(doc) {
+    //Check to see if this user is a customer. If not, redirect them to their dashboard.
+    firestore.doc("/Users/" + email).get().then(function (doc) {
 
-        if(doc.exists){
-                
-          var docData = doc.data();
-          role = docData.UserRole;
+      if (doc.exists) {
 
-          //Redirect user to the dashboard for their role.
-          if(role === "Customer") return;
-          else if(role === "Manager") window.location.replace("manager.html");
-          else if (role === "Deliverer") window.location.replace("deliverer.html");
-          else console.log("The value of role is not an accepted value: -" + role + ".");
+        var docData = doc.data();
+        role = docData.UserRole;
 
-        } else console.log("The users document does not exist.");
+        //Redirect user to the dashboard for their role.
+        if (role === "Customer") return;
+        else if (role === "Manager") window.location.replace("manager.html");
+        else if (role === "Deliverer") window.location.replace("deliverer.html");
+        else console.log("The value of role is not an accepted value: -" + role + ".");
 
-      });
-    
-    } else {
-      // No user is signed in. Redirect them to the homepage.
-      console.log("No user is signed in, redirecting...");
-      window.location.replace("homepage.html");
-    }
+      } else console.log("The users document does not exist.");
+
+    });
+
+  } else {
+    // No user is signed in. Redirect them to the homepage.
+    console.log("No user is signed in, redirecting...");
+    window.location.replace("homepage.html");
+  }
 });
