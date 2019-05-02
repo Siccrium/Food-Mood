3925f86df02fcffd8a64b202626f809b362729cb
Swati Adhikari <sadhika7@go.olemiss.edu>
2019-05-01 21:51:03 -0500

    Updated card style

--- js/customer.js
+++ js/customer.js
@@ -48,7 +48,7 @@ function renderRestaurants() {
       div.innerHTML = "<h3 style='color:#006400;'>" + data.RestaurantName + "</h3>"
         + "<button id='" + doc.id + "' type=submit class='btn btn-success'>View Restaurant</button>";
       // div.className = 'col-md-3 col-lg-10 mx-left text-center mb-3 card card-body d-inline-block font-weight-bold';
-      div.className = 'card card-body fixed float-left font-weight-bold';
+      div.className = 'card card-body fixed space float-left font-weight-bold';
       searchSection.appendChild(div);
       restaurants.push(data);
       restIDs.push(doc.id);
